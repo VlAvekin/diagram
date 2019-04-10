b2c88c267012a2993436396adb794ec778636a79
VlAvekin <vlad2002cat95git>
2019-04-10 21:45:11 +0300

    Init project

---
+++ src/main/java/com/vladavekin/diagram/DiagramApplication.java
@@ -0,0 +1,15 @@
+package com.vladavekin.diagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagramApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(DiagramApplication.class, args);
+    }
+
+}
